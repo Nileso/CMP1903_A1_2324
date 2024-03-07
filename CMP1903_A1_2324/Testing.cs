fc9c466c757179f14fcde08657d14f899d2eb8c9
OliverN <139251982+Nileso@users.noreply.github.com>
2024-03-07 10:55:05 +0000

    Update Testing.cs
    added comments for test die and debug.assert() lines

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -11,17 +11,17 @@ namespace CMP1903_A1_2324
             * This class should test the Game and the Die class.
             * Create a Game object, call the methods and compare their output to expected output.*/
 
-            Game game = new Game();
+            Game game = new Game()//creates a game object
             int[] nums = game.ReturnRolls();
             int sumOfNums = nums[0] + nums[1] + nums[2];
-            Debug.Assert(sumOfNums == game.SumTheDice(), "Error: The sums do not match");
+            Debug.Assert(sumOfNums == game.SumTheDice(), "Error: The sums do not match"); //compares the two sums to check for an error
 
             /* Create a Die object and call its method.
             * Use debug.assert() to make the comparisons and tests.*/
 
-            Die dieTest = new Die();
+            Die dieTest = new Die(); //creates a new die for test purposes
             dieTest.Roll();
-            Debug.Assert(dieTest.currentValue >= 1 && dieTest.currentValue <= 6, "Error: Value is not between 1 and 6 inclusive");
+            Debug.Assert(dieTest.currentValue >= 1 && dieTest.currentValue <= 6, "Error: Value is not between 1 and 6 inclusive"); //checks if the dice rolls are between the correct values
         }
 
     }
